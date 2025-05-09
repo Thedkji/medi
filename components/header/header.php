a04ce4d0de68056ae4275c88b9101072f4d5e021
CongTuyen04 <preintern.php.oct@gmail.com>
2025-05-09 17:51:47 +0700

    [tuyen]: menu medi

---
+++ components/header/header.php
@@ -0,0 +1,143 @@
+<link rel="stylesheet" href="./assets/css/header.css" />
+<script src="./assets/js/header.js"></script>
+    <header id="header" class="header has-transparent has-sticky sticky-jump transparent" style="">
+        <div class="header-wrapper">
+            <div id="masthead" class="header-main ">
+                <div class="header-inner flex-row container logo-left" role="navigation">
+
+                    <!-- Logo -->
+                    <div id="logo" class="flex-col logo">
+                        <!-- Header logo -->
+                        <a href="https://phanmemmedi.com/" title="Phần mềm quản lý phòng khám MEDi" rel="home">
+                            <img width="512" height="126" src="https://phanmemmedi.com/wp-content/uploads/2023/09/medring.png" class="header_logo header-logo" alt="Phần mềm quản lý phòng khám MEDi">
+                            <!-- <img width="512" height="126" src="https://phanmemmedi.com/wp-content/uploads/2023/09/medring.png" class="header-logo-dark" alt="Phần mềm quản lý phòng khám MEDi"> -->
+                        </a>
+                    </div>
+
+                    <!-- Mobile Left Elements -->
+                    <div class="flex-col show-for-medium flex-left">
+                        <ul class="mobile-nav nav nav-left "></ul>
+                    </div>
+
+                    <!-- Left Elements -->
+                    <div class="flex-col hide-for-medium flex-left flex-grow">
+                        <ul class="header-nav header-nav-main nav nav-left  nav-pills nav-size-large nav-uppercase"></ul>
+                    </div>
+
+                    <!-- Right Elements -->
+                    <div class="flex-col hide-for-medium flex-right">
+                        <ul class="desktop-nav header-nav header-nav-main nav nav-right  nav-pills nav-size-large nav-uppercase">
+                            <li id="menu-item-764" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-764 menu-item-design-default"><a href="https://phanmemmedi.com/gioi-thieu/" class="nav-top-link">Giới thiệu</a></li>
+                            <li id="menu-item-824" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-824 menu-item-design-default"><a href="https://phanmemmedi.com/tinh-nang/" class="nav-top-link">Tính năng</a></li>
+                            <li id="menu-item-904" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-904 menu-item-design-default has-dropdown"><a href="#" class="nav-top-link" aria-expanded="false" aria-haspopup="menu">Sản phẩm<i class="icon-angle-down"></i></a>
+                                <!-- <ul class="sub-menu nav-dropdown nav-dropdown-default">
+                                    <li id="menu-item-905" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-905"><a href="https://phanmemmedi.com/medi-phan-mem-quan-ly-phong-kham-da-khoa/">MEDi Phần mềm quản lý phòng khám Đa Khoa</a></li>
+                                    <li id="menu-item-956" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-956"><a href="https://phanmemmedi.com/medi-phan-mem-quan-ly-phong-kham-dong-y-y-hoc-co-truyen/">MEDi phần mềm quản lý phòng khám Đông y &amp; Y học cổ truyền</a></li>
+                                    <li id="menu-item-957" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-957"><a href="https://phanmemmedi.com/medi-phan-mem-quan-ly-phong-kham-vat-ly-tri-lieu-va-phuc-hoi-chuc-nang/">MEDi phần mềm quản lý phòng khám Vật lý trị liệu và Phục hồi chức năng</a></li>
+                                    <li id="menu-item-958" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-958"><a href="https://phanmemmedi.com/medi-phan-mem-quan-ly-phong-kham-khoa-tam-than/">MEDi phần mềm quản lý phòng khám khoa Tâm Thần</a></li>
+                                    <li id="menu-item-959" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-959"><a href="https://phanmemmedi.com/medi-phan-mem-quan-ly-phong-kham-ngoai/">MEDi phần mềm quản lý phòng khám Ngoại</a></li>
+                                    <li id="menu-item-960" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-960"><a href="https://phanmemmedi.com/medi-phan-mem-quan-ly-phong-kham-noi-tong-hop/">MEDi phần mềm quản lý phòng khám Nội Tổng Hợp</a></li>
+                                    <li id="menu-item-961" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-961"><a href="https://phanmemmedi.com/medi-phan-mem-quan-ly-phong-kham-tai-mui-hong/">MEDi phần mềm quản lý phòng khám Tai Mũi Họng</a></li>
+                                    <li id="menu-item-962" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-962"><a href="https://phanmemmedi.com/medi-phan-mem-quan-ly-phong-kham-xuong-khop/">MEDi phần mềm quản lý phòng khám Xương Khớp</a></li>
+                                    <li id="menu-item-963" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-963"><a href="https://phanmemmedi.com/medi-phan-mem-quan-ly-phong-kham-da-lieu/">MEDi phần mềm quản lý phòng khám Da Liễu</a></li>
+                                    <li id="menu-item-964" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-964"><a href="https://phanmemmedi.com/medi-phan-mem-quan-ly-phong-kham-san-phu-khoa/">MEDi phần mềm quản lý phòng khám Sản Phụ Khoa</a></li>
+                                    <li id="menu-item-965" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-965"><a href="https://phanmemmedi.com/medi-phan-mem-quan-ly-phong-kham-nha-khoa/">MEDi phần mềm quản lý phòng khám Nha Khoa</a></li>
+                                    <li id="menu-item-966" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-966"><a href="https://phanmemmedi.com/medi-phan-mem-quan-ly-phong-kham-nhi/">MEDi phần mềm quản lý phòng khám Nhi</a></li>
+                                </ul> -->
+                            </li>
+                            <li id="menu-item-1036" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1036 menu-item-design-default"><a href="https://phanmemmedi.com/bang-gia/" class="nav-top-link">Bảng giá</a></li>
+                            <li id="menu-item-1223" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1223 menu-item-design-default"><a href="https://phanmemmedi.com/video/" class="nav-top-link">Video</a></li>
+                            <li id="menu-item-1270" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-1270 menu-item-design-default"><a href="https://phanmemmedi.com/blog/" class="nav-top-link">Blog</a></li>
+                            <li id="menu-item-1021" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1021 menu-item-design-default"><a href="https://phanmemmedi.com/lien-he/" class="nav-top-link">Liên hệ</a></li>
+                            <li class="html header-button-2">
+                                <div class="header-button">
+                                    <a rel="noopener noreferrer" href="/dang-ky-dung-thu/" target="_blank" class="button secondary is-gloss is-large" style="border-radius:99px;">
+                                        <span>Đăng ký dùng thử</span>
+                                    </a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <!-- Mobile Right Elements -->
+                    <div class="flex-col show-for-medium flex-right">
+                        <ul class="mobile-nav nav nav-right ">
+                            <li class="html header-button-2">
+                                <div class="header-button">
+                                    <a rel="noopener noreferrer" href="/dang-ky-dung-thu/" target="_blank" class="button secondary is-gloss is-large" style="border-radius:99px;">
+                                        <span>Đăng ký dùng thử</span>
+                                    </a>
+                                </div>
+                            </li>
+                            <li class="nav-icon has-icon">
+                                <a href="#" data-open="#main-menu" data-pos="left" data-bg="main-menu-overlay" data-color="" class="is-small" aria-label="Menu" aria-controls="main-menu" aria-expanded="false">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+                                    </svg>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div id="mobile-menu-overlay" class="mobile-menu-overlay">
+                        <div class="mobile-menu-content">
+                            <ul>
+                                <li class="has-submenu"><a href="https://phanmemmedi.com/gioi-thieu/">Giới thiệu</a></li>
+                                <li class="has-submenu"><a href="https://phanmemmedi.com/tinh-nang/">Tính năng</a></li>
+                                <li class="has-submenu">
+                                    <a href="#">Sản phẩm 
+                                        <span class="submenu-toggle">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                                                <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
+                                            </svg>
+                                        </span>
+                                    </a>
+                                    <ul class="submenu">
+                                        <!-- dán class has-submenu nếu muốn thêm cấp -->
+                                        <li class="has-submenu">
+                                            <a href="#">Sản phẩm 
+                                                <span class="submenu-toggle">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                                                        <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
+                                                    </svg>
+                                                </span>
+                                            </a>
+                                            <ul class="submenu">
+                                                <li><a href="#">Sản phẩm 1</a></li>
+                                                <li><a href="#">Sản phẩm 2</a></li>
+                                                <li><a href="#">Sản phẩm 3</a></li>
+                                            </ul>
+                                        </li>
+                                        <!-- end cấp 2 -->
+                                        <li><a href="#">Sản phẩm 2</a></li>
+                                        <li><a href="#">Sản phẩm 3</a></li>
+                                    </ul>
+                                </li>
+                                <li class="has-submenu"><a href="#">Sản phẩm</a></li>
+                                <li class="has-submenu"><a href="https://phanmemmedi.com/bang-gia/">Bảng giá</a></li>
+                                <li class="has-submenu"><a href="https://phanmemmedi.com/video/">Video</a></li>
+                                <li class="has-submenu"><a href="https://phanmemmedi.com/blog/">Blog</a></li>
+                                <li class="has-submenu"><a href="https://phanmemmedi.com/lien-he/">Liên hệ</a></li>
+                                <li>
+                                    <a rel="noopener noreferrer" href="/dang-ky-dung-thu/" target="_blank" class="button secondary is-gloss is-large btn-mobile" style="border-radius:99px;">
+                                        <span>Đăng ký dùng thử</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <span class="close-mobile-menu">&times;</span>
+                    </div>
+                </div>
+
+                <div class="container">
+                    <div class="top-divider full-width"></div>
+                </div>
+            </div>
+
+            <div class="header-bg-container fill">
+                <div class="header-bg-image fill"></div>
+                <div class="header-bg-color fill"></div>
+            </div>	
+        </div>
+    </header>
+    
