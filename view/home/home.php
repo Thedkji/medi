7326fead22d6cf9af3d06b1040fa43215c3d5b71
Quang <nmquang2303@gmail.com>
2025-05-09 17:51:55 +0700

    [quang] : 50% trang home chưa reponsive

--- view/home/home.php
+++ view/home/home.php
@@ -1,5 +1,36 @@
 <link rel="stylesheet" href="./assets/css/home.css">
 <script src="./assets/js/home/banner-header.js"></script>
+<script src="./assets/js/home/medi-body-feature-show.js"></script>
+<style>
+    #medi-home .medi-body .content-item li {
+        list-style: none;
+        background-repeat: no-repeat;
+        padding-left: 30px;
+        background-image: url('./assets/imgs/li.png');
+        height: 30px;
+    }
+
+    #medi-home .option-container .option-item button {
+        width: 100%;
+        height: 100%;
+        border: none;
+        border-radius: 10px;
+        background-image: url('./assets/imgs/Frame-162-1.png');
+        background-repeat: no-repeat;
+        background-size: 100% 100%;
+    }
+
+    #medi-home .option-container .option-item span {
+        font-size: 16px;
+        display: inline-block;
+        color: white;
+        font-weight: bold;
+        width: 100px;
+        margin-right: 120px;
+        text-align: start;
+        line-height: 25px;
+    }
+</style>
 <div id="medi-home">
     <div class="medi-banner">
         <div class="swiper">
@@ -75,5 +106,171 @@
                 <p>Khách hàng đăng ký<br>dùng thử</p>
             </div>
         </section>
+    </div>
+
+    <div class="medi-body container">
+        <h1>MEDi – SỰ LỰA CHỌN HOÀN HẢO CHO PHÒNG KHÁM CỦA BẠN</h1>
+        <div class="content">
+            <img src="./assets/imgs/luachon-medi.png" alt="">
+            <div class="content-item">
+                <h2>NÂNG CAO 30% HIỆU SUẤT CÔNG VIỆC</h2>
+                <ul>
+                    <li>Tăng tốc độ xử lý hồ sơ bệnh án</li>
+                    <li>Tiết kiệm thời gian, không trùng lịch khám, hiệu suất không bị gián đoạn.</li>
+                    <li>Tích hợp đầy đủ tính năng: Quản lý lịch hẹn, Quản lý thăm khám khách hàng, Quản lý kho thuốc, Quản lý liệu trình, Quản lý báo cáo,…</li>
+                </ul>
+            </div>
+        </div>
+        <div class="content">
+            <div class="content-item">
+                <h2>NÂNG CAO 30% HIỆU SUẤT CÔNG VIỆC</h2>
+                <ul>
+                    <li>Tăng tốc độ xử lý hồ sơ bệnh án</li>
+                    <li>Tiết kiệm thời gian, không trùng lịch khám, hiệu suất không bị gián đoạn.</li>
+                    <li>Tích hợp đầy đủ tính năng: Quản lý lịch hẹn, Quản lý thăm khám khách hàng, Quản lý kho thuốc, Quản lý liệu trình, Quản lý báo cáo,…</li>
+                </ul>
+            </div>
+            <img src="./assets/imgs/man-hinh-quan-ly-thu-chi.png" alt="">
+        </div>
+        <div class="content">
+            <img src="./assets/imgs/man-hinh-nang-cao-chat-luong-dich-vu-phong-kham.png" alt="">
+            <div class="content-item">
+                <h2>NÂNG CAO 30% HIỆU SUẤT CÔNG VIỆC</h2>
+                <ul>
+                    <li>Tăng tốc độ xử lý hồ sơ bệnh án</li>
+                    <li>Tiết kiệm thời gian, không trùng lịch khám, hiệu suất không bị gián đoạn.</li>
+                    <li>Tích hợp đầy đủ tính năng: Quản lý lịch hẹn, Quản lý thăm khám khách hàng, Quản lý kho thuốc, Quản lý liệu trình, Quản lý báo cáo,…</li>
+                </ul>
+            </div>
+        </div>
+        <div class="content">
+            <div class="content-item">
+                <h2>NÂNG CAO 30% HIỆU SUẤT CÔNG VIỆC</h2>
+                <ul>
+                    <li>Tăng tốc độ xử lý hồ sơ bệnh án</li>
+                    <li>Tiết kiệm thời gian, không trùng lịch khám, hiệu suất không bị gián đoạn.</li>
+                    <li>Tích hợp đầy đủ tính năng: Quản lý lịch hẹn, Quản lý thăm khám khách hàng, Quản lý kho thuốc, Quản lý liệu trình, Quản lý báo cáo,…</li>
+                </ul>
+            </div>
+            <img src="./assets/imgs/giai-quyet-95-van-de-thu-cong.png" alt="">
+        </div>
+        <div class="content">
+            <img src="./assets/imgs/X2-trai-nghiem-khach-hang.png" alt="">
+            <div class="content-item">
+                <h2>NÂNG CAO 30% HIỆU SUẤT CÔNG VIỆC</h2>
+                <ul>
+                    <li>Tăng tốc độ xử lý hồ sơ bệnh án</li>
+                    <li>Tiết kiệm thời gian, không trùng lịch khám, hiệu suất không bị gián đoạn.</li>
+                    <li>Tích hợp đầy đủ tính năng: Quản lý lịch hẹn, Quản lý thăm khám khách hàng, Quản lý kho thuốc, Quản lý liệu trình, Quản lý báo cáo,…</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="medi-body-button">
+        <div>
+            <a href="###">
+                <button>15 ngày miễn phí</button>
+            </a>
+            <a href="###">
+                <button>Đăng ký ngay</button>
+            </a>
+        </div>
+    </div>
+
+    <div class="medi-body medi-body-option">
+        <h1>MEDi LÀ LỰA CHỌN HÀNG ĐẦU CỦA PHÒNG KHÁM ĐA KHOA, CHUYÊN KHOA</h1>
+
+        <div class="option-container container">
+            <div class="option-item">
+                <a href="###">
+                    <button>
+                        <span>Phòng khám Đa khoa</span>
+                    </button>
+                </a>
+            </div>
+            <div class="option-item">
+                <a href="###">
+                    <button>
+                        <span>Phòng khám Đa khoa</span>
+                    </button>
+                </a>
+            </div>
+            <div class="option-item">
+                <a href="###">
+                    <button>
+                        <span>Phòng khám Đa khoa</span>
+                    </button>
+                </a>
+            </div>
+            <div class="option-item">
+                <a href="###">
+                    <button>
+                        <span>Phòng khám Đa khoa</span>
+                    </button>
+                </a>
+            </div>
+            <div class="option-item">
+                <a href="###">
+                    <button>
+                        <span>Phòng khám Đa khoa</span>
+                    </button>
+                </a>
+            </div>
+            <div class="option-item">
+                <a href="###">
+                    <button>
+                        <span>Phòng khám Đa khoa</span>
+                    </button>
+                </a>
+            </div>
+            <div class="option-item">
+                <a href="###">
+                    <button>
+                        <span>Phòng khám Đa khoa</span>
+                    </button>
+                </a>
+            </div>
+            <div class="option-item">
+                <a href="###">
+                    <button>
+                        <span>Phòng khám Đa khoa</span>
+                    </button>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="medi-body medi-body-feature-show">
+        <h1>Tính năng ưu việt của phần mềm MEDi</h1>
+        <div class="swiper">
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <img src="./assets/imgs/Quan-ly-lich-hen-kham.png" alt="">
+                </div>
+                <div class="swiper-slide">
+                    <img src="./assets/imgs/Quan-ly-lich-hen-kham.png" alt="">
+                </div>
+                <div class="swiper-slide">
+                    <img src="./assets/imgs/Quan-ly-lich-hen-kham.png" alt="">
+                </div>
+                <div class="swiper-slide">
+                    <img src="./assets/imgs/Quan-ly-lich-hen-kham.png" alt="">
+                </div>
+                <div class="swiper-slide">
+                    <img src="./assets/imgs/Quan-ly-lich-hen-kham.png" alt="">
+                </div>
+                <div class="swiper-slide">
+                    <img src="./assets/imgs/Quan-ly-lich-hen-kham.png" alt="">
+                </div>
+                <div class="swiper-slide">
+                    <img src="./assets/imgs/Quan-ly-lich-hen-kham.png" alt="">
+                </div>
+            </div>
+            <div class="swiper-pagination"></div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
     </div>
 </div>
