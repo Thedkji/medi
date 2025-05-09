5af45c94b20b244786b7ef73cac01521abe75c7e
Quang <nmquang2303@gmail.com>
2025-05-09 16:30:12 +0700

    [quang] : hoàn thiện medi-banner và medi-section ở home

--- view/home/home.php
+++ view/home/home.php
@@ -1,3 +1,79 @@
-<div class="container">
-    <div>123</div>
+<link rel="stylesheet" href="./assets/css/home.css">
+<script src="./assets/js/home/banner-header.js"></script>
+<div id="medi-home">
+    <div class="medi-banner">
+        <div class="swiper">
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <a href="###">
+                        <img src="./assets/imgs/banner-med2.png" alt="">
+                    </a>
+                    <div class="text-inner container">
+                        <h1 class="color-secondary">MEDi – Phần Mềm Nhật Bản Quản lý Phòng Khám &amp; Nhà Thuốc Hiệu Quả</h1>
+                        <p>&nbsp;</p>
+                        <h2 class="color-primary my-20">Giá chỉ từ<br>350.000 VNĐ/tháng</h2>
+                        <a href="/dang-ky-dung-thu/" target="_self">
+                            <span>Dùng thử MIỄN PHÍ ngay</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <a href="###">
+                        <img src="./assets/imgs/banner-tang-tranh-trang-tri-phong-kham-1400x512.png" alt="">
+                    </a>
+                </div>
+                <div class="swiper-slide">
+                    <a href="###">
+                        <img src="./assets/imgs/Frame-45-4-1400x536.png" alt="">
+                    </a>
+                    <div class="text-inner container">
+                        <div id="text-138976528" class="text h1-medi-ldp">
+                            <h1>MEDi – Phần mềm quản lý phòng khám</h1>
+                            <h1>Nha Khoa chuẩn Nhật All In One</h1>
+                        </div>
+                        <p>&nbsp;</p>
+                        <div class="my-20">
+                            <h2 class="color-primary">Giá chỉ từ 350.000 VNĐ/tháng</h2>
+                            <h2 class="color-secondary">Mua 12 tháng tặng 3 tháng</h2>
+                        </div>
+                        <a href="/phan-mem-quan-ly-phong-kham-nha-khoa-uu-dai/" target="_self">
+                            <span>Liên hệ ngay</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <a href="###">
+                        <img src="./assets/imgs/Frame-45-9.png" alt="">
+                    </a>
+                </div>
+
+            </div>
+            <div class=" swiper-pagination">
+            </div>
+        </div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+    </div>
+
+    <div class="medi-section">
+        <section class="container">
+            <div>
+                <h2 class="item-number">428</h2>
+                <p>Khách hàng đăng ký<br>dùng thử</p>
+            </div>
+            <div>
+                <h2 class="item-number">428</h2>
+                <p>Khách hàng đăng ký<br>dùng thử</p>
+            </div>
+            <div>
+                <h2 class="item-number">428</h2>
+                <p>Khách hàng đăng ký<br>dùng thử</p>
+            </div>
+            <div>
+                <h2 class="item-number">428</h2>
+                <p>Khách hàng đăng ký<br>dùng thử</p>
+            </div>
+        </section>
+    </div>
 </div>
