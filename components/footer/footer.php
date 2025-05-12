d90515c509b661777540e175b144634a63b2f274
Quang <nmquang2303@gmail.com>
2025-05-12 10:46:41 +0700

    [quang] : cập nhật sửa lỗi giao diện trong footer

--- components/footer/footer.php
+++ components/footer/footer.php
@@ -1,64 +1,22 @@
 <link rel="stylesheet" href="./assets/css/footer.css" />
-<div class="row rowabs hide-for-small" id="row-1414367170">
-    <div id="col-2025112393" class="col small-12 large-12">
-        <div class="col-inner">
-            <div class="banner has-hover luantesst" id="banner-1727893074">
-                <div class="banner-inner fill">
-                    <div class="banner-bg fill">
-                        <div class="bg fill bg-fill bg-loaded"></div>
-                        <div class="is-border"></div>
-                    </div>
-                    <div class="banner-layers container">
-                        <div class="fill banner-link"></div>            
-                        <div id="text-box-1449854445" class="text-box banner-layer x50 md-x50 lg-x50 y50 md-y50 lg-y50 res-text">
-                            <div class="text-box-content text dark">
-                                <div class="text-inner text-center">
-                                    <h3><strong>MEDi – TRỢ THỦ ĐẮC LỰC CHO PHÒNG KHÁM CỦA BẠN</strong></h3>
-                                    <div id="gap-2097714087" class="gap-element clearfix"></div>
-                                    <div class="wpcf7 js" id="wpcf7-f1175-p70-o1" lang="en-US" dir="ltr">
-                                        <div class="screen-reader-response"><p role="status" aria-live="polite" aria-atomic="true"></p> <ul></ul></div>
-                                        <form action="/#wpcf7-f1175-p70-o1" method="post" class="wpcf7-form init" aria-label="Contact form" novalidate="novalidate" data-status="init">
-                                            <div class="hidden-form-inputs">
-                                                <input type="hidden" name="_wpcf7" value="1175">
-                                                <input type="hidden" name="_wpcf7_version" value="5.7">
-                                                <input type="hidden" name="_wpcf7_locale" value="en_US">
-                                                <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f1175-p70-o1">
-                                                <input type="hidden" name="_wpcf7_container_post" value="70">
-                                                <input type="hidden" name="_wpcf7_posted_data_hash" value="">
-                                            </div>
-                                            <div class="flex-row form-flat medium-flex-wrap">
-                                                <p><br></p>
-                                                <div class="flex-col flex-grow medring-footer-sub2">
-                                                    <p><br>
-                                                        <span class="wpcf7-form-control-wrap" data-name="your-email">
-                                                            <input size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Nhập email hoặc số điện thoại..." value="" type="text" name="your-email">
-                                                        </span>
-                                                        <input class="wpcf7-form-control has-spinner wpcf7-submit button-medring2" type="submit" value="Đăng ký ngay">
-                                                        <span class="wpcf7-spinner"></span><br>
-                                                    </p>
-                                                </div>
-                                                <p><br></p>
-                                            </div>
-                                            <div class="wpcf7-response-output" aria-hidden="true"></div>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="height-fix is-invisible">
-                    <img loading="lazy" decoding="async" width="1020" height="306" src="https://phanmemmedi.com/wp-content/uploads/2023/10/Frame-48.png" class="attachment-large size-large" alt="" srcset="https://phanmemmedi.com/wp-content/uploads/2023/10/Frame-48.png 1195w, https://phanmemmedi.com/wp-content/uploads/2023/10/Frame-48-800x240.png 800w, https://phanmemmedi.com/wp-content/uploads/2023/10/Frame-48-768x230.png 768w, https://phanmemmedi.com/wp-content/uploads/2023/10/Frame-48-600x180.png 600w" sizes="auto, (max-width: 1020px) 100vw, 1020px">
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
+<style>
+    footer .positon-footer {
+        background-image: url('./assets/imgs/banner_footer.png');
+        background-size: 100% 100%;
+        height: 261px;
+        width: 60%;
+        position: absolute;
+        top: -20%;
+        left: 50%;
+        transform: translateX(-50%);
+        padding: 30px 100px;
+    }
+</style>
+<!-- #region -->
 <footer id="footer" class="footer-wrapper">
     <!-- FOOTER 1 -->
     <!-- FOOTER 2 -->
-    <div class="footer-widgets footer footer-2 dark">
+    <div class="footer-widgets footer footer-2 dark container">
         <div class="row dark large-columns-2 mb-0">
             <div id="custom_html-3" class="widget_text col pb-0 widget widget_custom_html">
                 <div class="textwidget custom-html-widget">
@@ -68,7 +26,10 @@
                     <p>Trụ sở: Tầng 7 tòa nhà CTM Complex, 139 Cầu Giấy, Hà Nội <br> VP Hồ Chí Minh: Cobi Tower II, số 2-4, Đường 8, p.3, q.7, Tp. HCM</p>
                     <div class="subcribe-footer">
                         <div class="wpcf7 js" id="wpcf7-f15-o3" lang="en-US" dir="ltr">
-                            <div class="screen-reader-response"><p role="status" aria-live="polite" aria-atomic="true"></p> <ul></ul></div>
+                            <div class="screen-reader-response">
+                                <p role="status" aria-live="polite" aria-atomic="true"></p>
+                                <ul></ul>
+                            </div>
                             <form action="/#wpcf7-f15-o3" method="post" class="wpcf7-form init" aria-label="Contact form" novalidate="novalidate" data-status="init">
                                 <div style="display: none;">
                                     <input type="hidden" name="_wpcf7" value="15">
@@ -121,7 +82,7 @@
                                 <div class="ux-menu-link flex menu-item">
                                     <a class="ux-menu-link__link flex" href="#">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill ux-menu-link__icon" viewBox="0 0 16 16">
-                                            <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
+                                            <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
                                         </svg>
                                         <span class="ux-menu-link__text">086 201 8966</span>
                                     </a>
@@ -129,7 +90,7 @@
                                 <div class="ux-menu-link flex menu-item">
                                     <a class="ux-menu-link__link flex" href="#">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope ux-menu-link__icon" viewBox="0 0 16 16">
-                                            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
+                                            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
                                         </svg>
                                         <span class="ux-menu-link__text">sales@medring.co.jp</span>
                                     </a>
@@ -202,6 +163,22 @@
         </div>
     </div> -->
     <a href="#top" class="back-to-top button icon invert plain fixed bottom z-1 is-outline hide-for-medium circle active" id="top-link" aria-label="Go to top"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-compact-up" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M7.776 5.553a.5.5 0 0 1 .448 0l6 3a.5.5 0 1 1-.448.894L8 6.56 2.224 9.447a.5.5 0 1 1-.448-.894z"/>
-</svg></a>
+            <path fill-rule="evenodd" d="M7.776 5.553a.5.5 0 0 1 .448 0l6 3a.5.5 0 1 1-.448.894L8 6.56 2.224 9.447a.5.5 0 1 1-.448-.894z" />
+        </svg></a>
+
+    <div class="positon-footer">
+        <div class="title">
+            <h2>Clinic – TRỢ THỦ ĐẮC LỰC CHO PHÒNG KHÁM CỦA BẠN</h2>
+        </div>
+
+        <div class="input-email">
+            <input type="text" placeholder="Nhập email hoặc số điện thoại...">
+
+            <a href="###">
+                <button type="button">
+                    Đăng ký ngay
+                </button>
+            </a>
+        </div>
+    </div>
 </footer>
